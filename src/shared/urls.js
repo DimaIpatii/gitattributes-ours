177b8c8c007620fc7e08320d6294acf677b0320c
Dmytro Ipatii <dmytro.ipatii@aalto.it>
2022-05-02 11:42:39 +0200

    message

--- src/shared/urls.js
+++ src/shared/urls.js
@@ -1,2 +1,3 @@
-export const HOME_URL = "https://STG-home.it";
+export const HOME_URL = "https://STG-home.com";
 export const GALLERY_URL = "https://STG-gallery.it";
+export const SHOP_URL = "https://STG-shop.it";
